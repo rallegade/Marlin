09fe538cabea41bc30eca5ebd96af74284bd9d11
ralle.gade <ralle.gade@gmail.com>
2020-07-14 23:45:57 +0200

    Precise bed level probing
    Change to a precise bed level probing with a 5x5 grid, single probe and use extrapolate to try and guess correction curve after the probed grid.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1283,7 +1283,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
@@ -1293,7 +1293,7 @@
 
     // Beyond the probed grid, continue the implied tilt?
     // Default is to maintain the height of the nearest edge.
-    //#define EXTRAPOLATE_BEYOND_GRID
+    #define EXTRAPOLATE_BEYOND_GRID
 
     //
     // Experimental Subdivision of the grid by Catmull-Rom method.
