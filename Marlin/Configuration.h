02c7149cddf41c144f8f9166a14c03036c48d5a2
ralle.gade <ralle.gade@gmail.com>
2020-07-28 03:46:13 +0200

    Change name of the printer
    Rename to PretEnder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3"
+#define CUSTOM_MACHINE_NAME "PretEnder"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
