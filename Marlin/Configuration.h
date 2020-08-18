eeaef0ec926b550067358df53f5100834a0a102d
ralle.gade <ralle.gade@gmail.com>
2020-08-18 09:58:16 +0200

    Update author and machine name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "ralle.gade" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Rasmus Gade" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "PretEnder"
+#define CUSTOM_MACHINE_NAME "Prutender 3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
