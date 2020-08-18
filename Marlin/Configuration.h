b0c4bad1fd7cbbff217bdc7ae726e3fb099332b1
ralle.gade <ralle.gade@gmail.com>
2020-08-18 10:08:31 +0200

    Update buildarea
    Update the build area to reflect reality.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1146,7 +1146,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 215
-#define Y_BED_SIZE 215
+#define Y_BED_SIZE 225
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1154,7 +1154,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 275
+#define Z_MAX_POS 265
 
 /**
  * Software Endstops
