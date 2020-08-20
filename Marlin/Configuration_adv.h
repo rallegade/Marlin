aaff155eb8474b15f596ff0d43d0f9552503c6e5
ralle.gade <ralle.gade@gmail.com>
2020-08-20 14:12:12 +0200

    Enable 32 microstepping on E
    Enable 32 microstepping on E and change the steps/mm accordingly

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2285,7 +2285,7 @@
 
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT      550
-    #define E0_MICROSTEPS    16
+    #define E0_MICROSTEPS    32
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
   #endif
