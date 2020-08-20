d9f397f7a3360343639a1955e4a65a280c3797f2
ralle.gade <ralle.gade@gmail.com>
2020-08-20 14:17:40 +0200

    Typo in junction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -832,7 +832,7 @@
  *   https://reprap.org/forum/read.php?1,739819
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
-//#if DISABLED(CLASSIC_JERK)
+#if DISABLED(CLASSIC_JERK)
   #define JUNCTION_DEVIATION_MM 0.032 // (mm) Distance from real junction edge (this has been calculated from 0.4*jerk*jerk/acceleration which is 0.4*10*10/800 for this case)
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
