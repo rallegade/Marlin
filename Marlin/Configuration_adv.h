af171a0da8558c517eb01643d7c91f96ea607c47
ralle.gade <ralle.gade@gmail.com>
2020-08-23 15:57:37 +0200

    More current to Z axis

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2252,7 +2252,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       500
+    #define Z_CURRENT       580
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
