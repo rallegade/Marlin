74312e2fb62a9e0e5967b2c6e1906dc743706943
ralle.gade <ralle.gade@gmail.com>
2020-08-25 11:09:28 +0200

    Fix typos

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2216,12 +2216,12 @@
  */
 #if HAS_TRINAMIC_CONFIG
 
-  #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
-  #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
+  #define HOLD_MULTIPLIER    0.5   // Scales down the holding current from run current
+  #define INTERPOLATE       true   // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       500        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  200  // (mA) RMS current for sensorless homing
+    #define X_CURRENT       500    // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT_HOME  200    // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
