30dd662c0a420732f82013cbe7e7b51202b45432
ralle.gade <ralle.gade@gmail.com>
2020-09-01 10:23:05 +0200

    Update Z motor current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2252,7 +2252,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       900        //this is double the ammount as two motors are run in serial
+    #define Z_CURRENT       800        //this is double the ammount as two motors are run in serial
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
